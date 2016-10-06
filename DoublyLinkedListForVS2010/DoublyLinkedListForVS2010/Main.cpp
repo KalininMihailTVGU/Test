635283104df7217f01db7e3dffcea538553edc46
KalininMihailTVGU <kalinin.mihail.dmitrievich@gmail.com>
2016-10-06 19:00:38 +0300

    Add files via upload

---
+++ DoublyLinkedListForVS2010/DoublyLinkedListForVS2010/Main.cpp
@@ -0,0 +1,14 @@
+// DoublyLinkedListForVS2010.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "List.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
