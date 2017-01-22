4ee1455ae9f5dfbfd49218c448d15b759bdb1ca9
KalininMihailTVGU <kalinin.mihail.dmitrievich@gmail.com>
2017-01-22 14:18:46 +0300

    Function append

---
+++ String2/String.cpp
@@ -0,0 +1,48 @@
+// String.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include "String.h"
+
+
+int String::getLenght()
+{
+	return length;
+}
+
+char *String::getData()
+{
+	return data;
+}
+
+String::String(char *sData)
+{
+	length = strlen(sData)+1;
+	data = new char[length];
+	strcpy(data, sData);
+}
+
+String::~String()
+{
+	if (0 != data)
+	{
+		delete[] data;
+	}
+}
+
+void String::append(String str)
+{
+	int iNewLen = length+str.getLenght();
+	char *buffer = new char[iNewLen];
+	strcpy(buffer,data);
+	strcat(buffer,str.getData());
+	delete[] data;
+	data = new char[iNewLen];
+	strcpy(data, buffer);
+	delete[] buffer;
+	length = iNewLen;
+}
+
+
---
+++ String2/String.h
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+
+
+class String
+{
+private:
+	int length;
+	char *data;
+public:
+
+	int getLenght();
+
+	char *getData();
+
+	String(char *sData);
+
+	~String();
+
+	void append(String str);
+
+	bool insert(int &pos, String &str)
+	{
+		/*String *tmp1, tmp2; // объявляем временные переменные
+
+		if (pos > this->length) return false;
+
+//		tmp1 = this;
+		
+		int newLength = this->length + str.length;
+
+		for (int i = pos+1; i < this->length; i++)
+		{
+			tmp2.data[i] = this->data[i]; // сохраняем часть строки от места вставки до конца
+			
+		}
+
+		for (int i = pos+1; i < str.length; i++)
+		{
+			this->data[i] = str.data[i]; // вставляем нужную строку
+		}
+
+		for (int i = newLength + 1; i < newLength + str.length; i++)
+		{
+			this->data[i] = str.data[i]; // добавляем сохранённую часть строки
+		}
+
+		this->length += str.length; // увеличение длины исходной строки
+
+		return true;*/
+	}
+};
